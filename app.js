31c917e297b6f032d6205f12a34d29ba2984a4ec
John Alvarez <jcalvare@Johns-MacBook-Pro.local>
2020-03-04 16:46:53 -0500

    new branch genisis

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 //var twitterData = require('./twitterData');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var session = require("express-session"),
+    bodyParser = require("body-parser");
 
 var sampledb = { constellation:[ ]};
 
@@ -16,6 +20,13 @@ app.use('/controls', express.static(__dirname + '/controls'));
 app.use('/json', express.static(__dirname + '/public/json'));
 
 
+app.use(express.static("public"));
+app.use(session({ secret: "Tiqnm2d0FHUoc4nXDU1PNIj17p7kWdCs2uLiEpO2SFGXfZX9zT" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 
 app.set('view engine', 'ejs');
@@ -24,6 +35,18 @@ app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
 });
+
+passport.use(new TwitterStrategy({
+    consumerKey: 'SiPSBNbS3Z07JISLrMbpMytHG',
+    consumerSecret: 'Tiqnm2d0FHUoc4nXDU1PNIj17p7kWdCs2uLiEpO2SFGXfZX9zT',
+    callbackURL: "http://127.0.0.1:3000/starlights/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -44,6 +67,21 @@ app.get('/winterspark', function(req, res) {
 app.get('/starlights', function(req, res) {
 	res.render('starlights');
 });
+
+
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+ 
 
 
 
